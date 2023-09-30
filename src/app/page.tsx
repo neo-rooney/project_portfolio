17be0de1c9a13f63f35d79783f5e800d9f2af033
rooney <bch3454@gmail.com>
2023-09-30 14:58:34 +0900

    Feat: Create Header Component
    - Create TypeWriter Comp
    - Create Cursor Comp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
+import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <div>
+    <div className="w-full overflow-x-hidden bg-[#f1f6f9]">
       {/* navbar */}
       <Navbar />
       {/* header */}
+      <Header />
       {/* about */}
       {/* skills */}
       {/* blogs */}
