91af75cf2b4c799594eb2f515d2cb0d0f4c13dcd
rooney <bch3454@gmail.com>
2023-10-02 22:06:05 +0900

    Chore: 프로젝트 리뉴얼을 위한 셋업 완료

--- src/app/components/CursorComponent.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Cursor } from "react-simple-typewriter";
-type Props = {};
-
-const CursorComponent = (props: Props) => {
-  return <Cursor cursorBlinking={true} />;
-};
-
-export default CursorComponent;
