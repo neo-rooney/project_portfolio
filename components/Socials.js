ca669b46b0bd757e4aa6d0ab4f21aee240b24f42
rooney <bch3454@gmail.com>
2023-10-03 13:31:34 +0900

    Feat: Header 컴포넌트

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,34 @@
+// next link
+import Link from "next/link";
+
+// icons
+import { RiLinkedinLine, RiGithubLine, RiDribbbleLine } from "react-icons/ri";
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div className="flex items-center gap-x-5 text-2xl">
+      <Link
+        href={"https://github.com/neo-rooney"}
+        className="hover:text-accent transition-all duration-300"
+      >
+        <RiGithubLine />
+      </Link>
+      <Link
+        href={
+          "https://www.linkedin.com/in/%EC%B2%A0%ED%9B%88-%EB%B0%B0-602645241/"
+        }
+        className="hover:text-accent transition-all duration-300"
+      >
+        <RiLinkedinLine />
+      </Link>
+      <Link
+        href={"https://dribbble.com/bch3454"}
+        className="hover:text-accent transition-all duration-300"
+      >
+        <RiDribbbleLine />
+      </Link>
+    </div>
+  );
 };
 
 export default Socials;
